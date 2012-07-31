e036e88f6150c795ec14656a6d1142827a6560b9
Leonard Huang <lueotw@github.com>
2012-08-01 00:29:12 +0800

    Upgrade to Django 1.4.1, Mezzanine 1.1.4

--- local_settings.py
+++ local_settings.py
@@ -17,3 +17,85 @@ DATABASES = {
         "PORT": "",
     }
 }
+
+INSTALLED_APPS = (
+    
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.redirects",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
+    "django.contrib.staticfiles",
+    
+    'django.contrib.humanize',
+    
+    "mezzanine.boot",
+    "mezzanine.conf",
+    "mezzanine.core",
+    "mezzanine.generic",
+    "mezzanine.blog",
+    "mezzanine.forms",
+    "mezzanine.pages",
+    "mezzanine.galleries",
+    "mezzanine.twitter",
+    "mezzanine.accounts",
+    #"mezzanine.mobile",
+    
+    'pm',
+    'postman',
+
+    'pagination',
+    'notification',
+    'timezones',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.static",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "django.core.context_processors.tz",
+    "mezzanine.conf.context_processors.settings",
+    'postman.context_processors.inbox',
+)
+
+MIDDLEWARE_CLASSES = (
+    "mezzanine.core.middleware.UpdateCacheMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.redirects.middleware.RedirectFallbackMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "mezzanine.core.request.CurrentRequestMiddleware",
+    "mezzanine.core.middleware.TemplateForDeviceMiddleware",
+    "mezzanine.core.middleware.TemplateForHostMiddleware",
+    "mezzanine.core.middleware.AdminLoginInterfaceSelectorMiddleware",
+    # Uncomment the following if using any of the SSL settings:
+    # "mezzanine.core.middleware.SSLRedirectMiddleware",
+    "mezzanine.pages.middleware.PageMiddleware",
+    "mezzanine.core.middleware.FetchFromCacheMiddleware",
+    'pagination.middleware.PaginationMiddleware',
+)
+
+
+## POSTMAN
+
+# POSTMAN_DISALLOW_ANONYMOUS = True # default is False
+# POSTMAN_DISALLOW_MULTIRECIPIENTS = True # default is False
+# POSTMAN_DISALLOW_COPIES_ON_REPLY = True # default is False
+# POSTMAN_DISABLE_USER_EMAILING = True # default is False
+# POSTMAN_AUTO_MODERATE_AS = True # default is None
+# POSTMAN_NOTIFIER_APP = None # default is 'notification'
+# POSTMAN_MAILER_APP = None # default is 'mailer'
+# POSTMAN_AUTOCOMPLETER_APP = {
+    # 'name': '', # default is 'ajax_select'
+    # 'field': '', # default is 'AutoCompleteField'
+    # 'arg_name': '', # default is 'channel'
+    # 'arg_default': 'postman_friends', # no default, mandatory to enable the feature
+# } # default is {}
